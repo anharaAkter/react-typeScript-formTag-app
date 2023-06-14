e1409b71a1f3aa34668645aeed011d09c0459e22
anharaAkter <anhara.susoma@gmail.com>
2023-06-14 14:49:58 +0200

    customHook uselocal added with functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from "react-bootstrap"
 import {  Navigate, Route, Routes } from "react-router-dom"
 import { NewNote } from "./components/NewNote"
+import { useLocalStorage } from "./Hooks/useLocalStorage"
+import { useMemo } from "react"
+import {v4 as uuidv4} from 'uuid';
 
 
 export type Note = {
@@ -17,15 +20,41 @@ export type Tag = {
   id : string
   label : string
 }
+export type RawNote = {
+  id : string
+} & RawNoteData
+export type RawNoteData = {
+  title : string 
+  markdown : string
+  tagIds : string[]
+}
 
 function App() {
+const [notes,setNotes] = useLocalStorage<RawNote[]>("NOTES",[])
+const [tags,setTags] = useLocalStorage<Tag[]>("TAGS",[])
 
+
+const noteWithTags = useMemo(()=>{
+  return notes.map(note=>{
+    return { ...note,tags : tags.filter(tag=>note.tagIds.includes(tag.id))}
+  })
+},[notes,tags])
+ function onCreateNote(data: NoteData){
+  setNotes(prevNotes => {
+    return [...prevNotes,{...data,id: uuidv4(),tagIds: tags.map(tag => tag.id)},
+    ]
+
+  })
+ }
+ function addTag(tag : Tag){
+  setTags(prev => [...prev , tag])
+ }
   return (
     <Container className="my-4">
 
   <Routes>
     <Route path="/" element ={<h1>Home</h1>} />
-    <Route path="/new" element ={<NewNote />} />
+    <Route path="/new" element ={<NewNote onSubmit = {onCreateNote} onAddTag = {addTag} availableTags={tags}/>} />
     <Route path="/:id"> 
     <Route index element={<h1>Show</h1>}></Route>
     <Route path="edit" element={<h1>Edit</h1>}></Route>
@@ -37,7 +66,6 @@ function App() {
     </Container>
   )
 }
-  
-    
+export default App 
 
-export default App
+
--- src/components/NoteForm.tsx
+++ src/components/NoteForm.tsx
@@ -1,14 +1,17 @@
 import { FormEvent, useRef, useState } from "react";
 import { Button, Col, Form, Row, Stack } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import CreatableSelect from "react-select";
 import { NoteData, Tag } from "../App";
+import CreatableReactSelect from "react-select/creatable"
+import {v4 as uuidv4} from "uuid"
 
 type NoteFormProps = {
-    onSubmit : (data: NoteData)=>void
+    onSubmit : (data: NoteData)=>void,
+    onAddTag : (tag : Tag) =>void,
+    availableTags : Tag[]
 }
 
-export function NoteForm({onSubmit}:NoteFormProps){
+export function NoteForm({onSubmit,onAddTag,availableTags}:NoteFormProps){
     const titleRef = useRef<HTMLInputElement>(null)
     const markdownRef = useRef<HTMLTextAreaElement>(null)
     const [selectedTags,setSelectedTags] = useState<Tag[]>([])
@@ -17,8 +20,7 @@ export function NoteForm({onSubmit}:NoteFormProps){
         onSubmit({
             title : titleRef.current!.value,
             markdown : markdownRef.current!.value,
-            tags : [],
-
+            tags : selectedTags,
         })
     }
     
@@ -36,8 +38,17 @@ export function NoteForm({onSubmit}:NoteFormProps){
                     <Col>
                         <Form.Group controlId="tags">
                             <Form.Label>tags</Form.Label>
-                            <CreatableSelect value={selectedTags.map(tag =>{
+                            <CreatableReactSelect
+                            onCreateOption ={label=>{
+                                const newTag = {id : uuidv4(),label}
+                                onAddTag(newTag)
+                                setSelectedTags(prev =>[...prev,newTag])
+                            } }
+                             value={selectedTags.map(tag =>{
                                     return {label : tag.label, value : tag.id}
+                            })}
+                            options={availableTags.map(tag =>{
+                                return { label : tag.label, value : tag.id}
                             })}
                             onChange={onChangetags => {
                                 setSelectedTags(onChangetags.map(tag=>{
